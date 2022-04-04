c46714ba1bae86b67b8e7021dcf36351f9332678
ivan <shanin-i2011@Yandex.ru>
2022-04-04 17:05:10 +0300

    s

--- index.php
+++ index.php
@@ -26,10 +26,16 @@
 		if ( have_posts() ){
 			while ( have_posts() ){
 				the_post();
+				echo '<h2><a href=' . 
+				get_permalink().'>'.get_the_title().'</a></h2>';
+				echo 'Автор: '; the_author();
+				echo ' '; the_date();
+				echo ' '; the_time();
+				the_category();
+				the_post_thumbnail();
 
-				echo '<h3><a href="'. get_permalink() .'">'. get_the_title() .'</a></h3>';
 
-				echo get_the_excerpt();
+				the_content('Перейти к полной статье...');
 			}
 		}
 		// елси записей не найдено
